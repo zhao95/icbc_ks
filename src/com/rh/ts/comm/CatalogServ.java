8fe526b0ec53b75278c63fe1d6dbaca8d6abe3fe
朱晋磊 <739901717@qq.com>
2017-09-02 18:54:35 +0800

    优化目录

--- src/com/rh/ts/comm/CatalogServ.java
+++ src/com/rh/ts/comm/CatalogServ.java
@@ -5,9 +5,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.StopWatch;
 
 import com.rh.core.base.Bean;
 import com.rh.core.base.Context;
+import com.rh.core.org.DeptBean;
+import com.rh.core.org.mgr.OrgMgr;
+import com.rh.core.org.util.OrgConstant;
 import com.rh.core.serv.CommonServ;
 import com.rh.core.serv.OutBean;
 import com.rh.core.serv.ParamBean;
@@ -70,75 +74,113 @@ public class CatalogServ extends CommonServ {
 	 *            公司编码
 	 */
 	private void syncCatalog(String ctlgModules, String cmpyCode) {
-		List<Bean> addCatalogList = new ArrayList<Bean>();
 
 		String[] moduleArg = ctlgModules.split(",");
 
-		// 获取组织机构
+		// 获取机构
 		SqlBean sql = new SqlBean();
 		sql.and("CMPY_CODE", cmpyCode);
+		sql.and("DEPT_TYPE", 2); // 2 机构
 		sql.and("S_FLAG", 1);
 		List<Bean> deptList = ServDao.finds(ServMgr.SY_ORG_DEPT_ALL, sql);
 
-		addCatalogList = new ArrayList<Bean>();
+		if (deptList == null) {
+			return;
+		}
 
+		
+		StopWatch sw = new StopWatch();
+        sw.start();
+        
 		for (String mod : moduleArg) {
-			Bean catlogBean = getCatalogBean(mod);
 
-			if (deptList != null) {
+			for (Bean bean : deptList) {
 
-				for (Bean bean : deptList) {
-					Bean addCatalog = new Bean();
-					addCatalog.set("CTLG_MODULE", mod);
+				syncCatalog(bean, mod, ctlgModules);
+			}
 
-					if (!catlogBean.containsKey(bean.getStr("DEPT_CODE"))) {
+		}
+		
+		
+		System.out.println(sw.toString());
+		sw.stop();
 
-						String ctlgCodeH = mod + "-" + bean.getStr("DEPT_CODE");
+	}
 
-						String ctlgPCodeH = mod + "-"
-								+ bean.getStr("DEPT_PCODE");
+	private void syncCatalog(Bean odept, String mod, String ctlgModules) {
+		
+		List<Bean> addCatalogList = new ArrayList<Bean>();
 
-						addCatalog.set("CTLG_CODE", bean.getStr("DEPT_CODE"));
+		SqlBean sql = new SqlBean();
 
-						addCatalog.set("CTLG_CODE_H", ctlgCodeH);
+		sql.and("ODEPT_CODE", odept.getStr("DEPT_CODE"));
 
-						addCatalog.set("CTLG_NAME", bean.getStr("DEPT_NAME"));
+		sql.and("DEPT_TYPE", OrgConstant.DEPT_TYPE_DEPT);
 
-						if (StringUtils.isNotBlank(bean.getStr("DEPT_PCODE"))) {
+		sql.and("S_FLAG", 1);
 
-							addCatalog.set("CTLG_PCODE",
-									bean.getStr("DEPT_PCODE"));
+		sql.selects("DEPT_CODE,DEPT_NAME,ODEPT_CODE,DEPT_PCODE,DEPT_SORT,CODE_PATH");
 
-							addCatalog.set("CTLG_PCODE_H", ctlgPCodeH);
-						}
-						addCatalog.set("CTLG_SORT", bean.getStr("DEPT_SORT"));
+		List<Bean> list = ServDao.finds(ServMgr.SY_ORG_DEPT, sql);
 
-						String codePathH = "";
-						String codePath = bean.getStr("CODE_PATH");
+		if (list == null) {
+			list = new ArrayList<Bean>();
+		}
 
-						String[] pathArg = codePath.split("\\^");
+		list.add(odept);
 
-						for (String path : pathArg) {
-							if (StringUtils.isNotBlank(path)) {
-								codePathH += mod + "-" + path + "^";
-							}
-						}
-						addCatalog.set("CTLG_PATH_H", codePathH);
-						addCatalog.set("CTLG_PATH", codePath);
-						addCatalog.set("CTLG_SHARE", ctlgModules);
+		Bean catlogBean = getCatalogBean(mod, list);
 
-						addCatalog.set("S_FLAG", 1);
-						addCatalog.set("READ_FLAG", 1);
-						addCatalog.set("S_ATIME", DateUtils.getDatetime());
-						addCatalog.set("S_ODEPT", bean.getStr("ODEPT_CODE"));
-						addCatalog.set("S_USER", Context.getUserBean()
-								.getUser());
-						addCatalogList.add(addCatalog);
+		for (Bean dept : list) {
+
+			Bean addCatalog = new Bean();
+
+			addCatalog.set("CTLG_MODULE", mod);
+
+			if (!catlogBean.containsKey(dept.getStr("DEPT_CODE"))) {
+
+				String ctlgCodeH = mod + "-" + dept.getStr("DEPT_CODE");
+
+				String ctlgPCodeH = mod + "-" + dept.getStr("DEPT_PCODE");
+
+				addCatalog.set("CTLG_CODE", dept.getStr("DEPT_CODE"));
+
+				addCatalog.set("CTLG_CODE_H", ctlgCodeH);
+
+				addCatalog.set("CTLG_NAME", dept.getStr("DEPT_NAME"));
+
+				if (StringUtils.isNotBlank(dept.getStr("DEPT_PCODE"))) {
+
+					addCatalog.set("CTLG_PCODE", dept.getStr("DEPT_PCODE"));
+
+					addCatalog.set("CTLG_PCODE_H", ctlgPCodeH);
+				}
+				addCatalog.set("CTLG_SORT", dept.getStr("DEPT_SORT"));
+
+				String codePathH = "";
+				String codePath = dept.getStr("CODE_PATH");
+
+				String[] pathArg = codePath.split("\\^");
+
+				for (String path : pathArg) {
+					if (StringUtils.isNotBlank(path)) {
+						codePathH += mod + "-" + path + "^";
 					}
 				}
+				addCatalog.set("CTLG_PATH_H", codePathH);
+				addCatalog.set("CTLG_PATH", codePath);
+				addCatalog.set("CTLG_SHARE", ctlgModules);
+
+				addCatalog.set("READ_FLAG", 1);
+				
+				addCatalogList.add(addCatalog);
 			}
+
 		}
-		ServDao.creates(servId, addCatalogList);
+
+		if (addCatalogList.size() > 0) {
+			ServDao.creates(servId, addCatalogList);
+		}
 	}
 
 	/**
@@ -146,17 +188,29 @@ public class CatalogServ extends CommonServ {
 	 * 
 	 * @param ctlgModule
 	 *            目录类型
+	 * @param odept
+	 *            机构id
 	 * @return
 	 */
-	private Bean getCatalogBean(String ctlgModule) {
+	private Bean getCatalogBean(String ctlgModule, List<Bean> list) {
 		Bean result = new Bean();
 
-		ParamBean paramBean = new ParamBean();
-		paramBean.set("CTLG_MODULE", ctlgModule);
-		paramBean.set("S_FLAG", 1);
-		List<Bean> ctlgList = ServDao.finds(servId, paramBean);
+		List<String> dlist = new ArrayList<String>();
+
+		for (Bean dept : list) {
+
+			dlist.add(dept.getStr("DEPT_CODE"));
+		}
+
+		SqlBean sql = new SqlBean();
+		sql.and("CTLG_MODULE", ctlgModule);
+		sql.andIn("CTLG_CODE", dlist.toArray());
+		sql.and("S_FLAG", 1);
+		List<Bean> ctlgList = ServDao.finds(servId, sql);
+
 		for (Bean ctlg : ctlgList) {
-			result.set(ctlg.getStr("CTLG_CODE"), ctlg);
+
+			result.set(ctlg.getStr("CTLG_CODE"), "");
 		}
 		return result;
 	}
@@ -183,7 +237,7 @@ public class CatalogServ extends CommonServ {
 			if (StringUtils.isNotBlank(addCode)) {
 				paramBean.set("CTLG_CODE_H", addModule + "-" + addCode);
 			}
-			
+
 			SqlBean sql = new SqlBean();
 			sql.and("CTLG_CODE", addPcode);
 			sql.and("CTLG_MODULE", addModule);
@@ -211,11 +265,11 @@ public class CatalogServ extends CommonServ {
 					String pathH = getCtlgPathH(addModule, codePath);
 					paramBean.set("CTLG_PATH_H", pathH);
 				}
-				
+
 				if (StringUtils.isBlank(addShare)) {
 					paramBean.set("CTLG_SHARE", addModule);
 				}
-				
+
 				paramBean.set("READ_FLAG", 2);
 			}
 		}
@@ -224,7 +278,7 @@ public class CatalogServ extends CommonServ {
 		 * 创建至其他模块，复制当前目录 至 目标模块
 		 */
 		if (addShare.length() > oldShare.length()) {
-			
+
 			String[] newArg = addShare.split(",");
 			String[] oldArg = oldShare.split(",");
 			String[] modArg = {};
@@ -254,11 +308,12 @@ public class CatalogServ extends CommonServ {
 
 	/**
 	 * 创建至其他模块,复制当前目录 至 目标模块
+	 * 
 	 * @param modArg
 	 * @param parentCtlg
 	 * @param paramBean
 	 */
-	private void addShareCatalog(String[] shareMod, String pCode,Bean paramBean) {
+	private void addShareCatalog(String[] shareMod, String pCode, Bean paramBean) {
 		if (shareMod == null || shareMod.length == 0) {
 			return;
 		}
@@ -268,60 +323,60 @@ public class CatalogServ extends CommonServ {
 			sql.and("CTLG_CODE", pCode);
 			sql.and("CTLG_MODULE", modCode);
 			sql.and("S_FLAG", 1);
-			//目标上级目录
+			// 目标上级目录
 			List<Bean> list = ServDao.finds(servId, sql);
 
 			if (list != null) {
 				Bean dataBean = new Bean();
-				
+
 				dataBean.set("CTLG_MODULE", modCode);
 				dataBean.set("CTLG_NAME", paramBean.getStr("CTLG_NAME"));
 				dataBean.set("CTLG_SHARE", paramBean.getStr("CTLG_SHARE"));
 				dataBean.set("CTLG_SORT", paramBean.getStr("CTLG_SORT"));
 				dataBean.set("CTLG_DESC", paramBean.getStr("CTLG_DESC"));
 				dataBean.set("CTLG_CODE", paramBean.getStr("CTLG_CODE"));
-				dataBean.set("CTLG_CODE_H", modCode+"-"+paramBean.getStr("CTLG_CODE"));
+				dataBean.set("CTLG_CODE_H", modCode + "-" + paramBean.getStr("CTLG_CODE"));
 				dataBean.set("CTLG_PCODE", paramBean.getStr("CTLG_PCODE"));
-				dataBean.set("CTLG_PCODE_H", modCode+"-"+paramBean.getStr("CTLG_PCODE"));
+				dataBean.set("CTLG_PCODE_H", modCode + "-" + paramBean.getStr("CTLG_PCODE"));
 				dataBean.set("CTLG_PATH", paramBean.getStr("CTLG_PATH"));
-				dataBean.set("CTLG_PATH_H", getCtlgPathH(modCode,paramBean.getStr("CTLG_PATH")));
-				
+				dataBean.set("CTLG_PATH_H", getCtlgPathH(modCode, paramBean.getStr("CTLG_PATH")));
+
 				dataBean.set("S_FLAG", 1);
 				dataBean.set("READ_FLAG", 2);
-				
+
 				ServDao.create(servId, dataBean);
 			}
 		}
 	}
-	
-	private Bean fillParamBean(ParamBean paramBean){
+
+	private Bean fillParamBean(ParamBean paramBean) {
 		Bean data = ServDao.find(servId, paramBean.getId());
-		
+
 		if (data != null) {
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_MODULE"))) {
 				data.set("CTLG_MODULE", paramBean.getStr("CTLG_MODULE"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_NAME"))) {
 				data.set("CTLG_NAME", paramBean.getStr("CTLG_NAME"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_SHARE"))) {
 				data.set("CTLG_SHARE", paramBean.getStr("CTLG_SHARE"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_SORT"))) {
 				data.set("CTLG_SORT", paramBean.getStr("CTLG_SORT"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_DESC"))) {
 				data.set("CTLG_DESC", paramBean.getStr("CTLG_DESC"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_CODE"))) {
 				data.set("CTLG_CODE", paramBean.getStr("CTLG_CODE"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_CODE_H"))) {
 				data.set("CTLG_CODE_H", paramBean.getStr("CTLG_CODE_H"));
 			}
@@ -329,42 +384,42 @@ public class CatalogServ extends CommonServ {
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_PCODE"))) {
 				data.set("CTLG_PCODE", paramBean.getStr("CTLG_PCODE"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_PCODE_H"))) {
 				data.set("CTLG_PCODE_H", paramBean.getStr("CTLG_PCODE_H"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_PATH"))) {
 				data.set("CTLG_PATH", paramBean.getStr("CTLG_PATH"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("CTLG_PATH_H"))) {
 				data.set("CTLG_PATH_H", paramBean.getStr("CTLG_PATH_H"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("S_FLAG"))) {
 				data.set("S_FLAG", paramBean.getStr("S_FLAG"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("READ_FLAG"))) {
 				data.set("READ_FLAG", paramBean.getStr("READ_FLAG"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("S_ATIME"))) {
 				data.set("S_ATIME", paramBean.getStr("S_ATIME"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("ODEPT_CODE"))) {
 				data.set("ODEPT_CODE", paramBean.getStr("ODEPT_CODE"));
 			}
-			
+
 			if (StringUtils.isNotBlank(paramBean.getStr("S_USER"))) {
 				data.set("S_USER", paramBean.getStr("S_USER"));
 			}
 		} else {
 			data = new Bean(paramBean);
 		}
-		
+
 		return data;
 	}
 
