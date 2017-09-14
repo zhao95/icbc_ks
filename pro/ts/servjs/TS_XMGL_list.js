7dba7016dc51837716f04da0926d4f301b46ccf8
lianxinshuai <934815805@qq.com>
2017-09-14 09:31:59 +0800

    项目

--- pro/ts/servjs/TS_XMGL_list.js
+++ pro/ts/servjs/TS_XMGL_list.js
@@ -9,6 +9,16 @@ var width = jQuery(window).width()-200;
 $(".rhGrid").find("tr").each(function(index, item) {
 	if(index != 0){
 		var XM_ID = item.id;
+		//判断显示按钮的情况
+		//if(){
+//			 $(item).find("td[icode='buttons']").append(
+//				     '<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_XMGL_look" actcode="look" title="查看" rowpk="'+XM_ID+'"><span class="rh-icon-inner"><span class="rh-icon-img btn-edit"></span></span></a>'+	
+//					 '<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_XMGL_copy" actcode="copy" title="复制" rowpk="'+XM_ID+'"><span class="rh-icon-inner"><span class="rh-icon-img btn-copy"></span></span></a>'+
+//					 '<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_XMGL_edit" actcode="edit" title="编辑" rowpk="'+XM_ID+'"><span class="rh-icon-inner"><span class="rh-icon-img btn-edit"></span></span></a>'+
+//					 '<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_XMGL_set" actcode="set"  title="设置"  rowpk="'+XM_ID+'"><span class="rh-icon-inner"><span class="rh-icon-img btn-option"></span></span></a>'+
+//					 '<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_XMGL_delete" actcode="delete"  title="删除" rowpk="'+XM_ID+'"><span class="rh-icon-inner"><span class="rh-icon-img btn-delete"></span></span></a>'
+//					 );	
+		//}else{
 	 $(item).find("td[icode='buttons']").append(
      '<div><span id="span_'+XM_ID+'" >....</span></div>'
 	 );
@@ -22,7 +32,7 @@ $(".rhGrid").find("tr").each(function(index, item) {
      '</div>'
      
      tableTag.append(btn); 
-	 
+		//}
 	 bindCard();
 	}
 });
@@ -39,44 +49,22 @@ var p=null;//div位置
 var spanLoc=null;//span位置
 function bindCard(){
 jQuery("td[icode='buttons'] span").unbind("mouseover").bind("mouseover", function(){
-	 d.empty();
-	 var tdTag =this;
-	 var kk = $(this).parent().parent().get(0).clientWidth;//操作td的宽度
-     var top = $(this).parent().parent().get(0).offsetTop; 
- 	 var pkCode = $(this).parent().parent().parent().children().get(2).innerText;
- 	 
- 	 var btn = $("#div_"+pkCode);
- 	
- 	 //btn.css("height":tdTag.height);
- 	 
- 	 d.append(btn.clone().css("display",""));
- 	 
-// 	var winWidth=windows.innerWidth;if (windows.innerWidth) {
-//        winWidth = windows.innerWidth; 
-//    }else if ((document.body) && (document.body.clientWidth)) {
-//        winWidth = document.body.clientWidth; 
-//}
-
- 	 dTag.style.display="block";
- 	 dTag.style.position="fixed";
- 	 dTag.style.left= document.body.clientWidth-220-kk/2+"px"; 
- 	 dTag.style.top=top+71+"px";
- 	 p=[dTag.offsetLeft,dTag.offsetTop,dTag.clientWidth,dTag.clientHeight];
- 	 var spanDiv= $(this).parent().get(0);
- 	 spanLoc=[spanDiv.offsetLeft,spanDiv.offsetTop,spanDiv.clientWidth,spanDiv.clientHeight];
- 	 d.focus();
+	 var pkCode = $(this).parent().parent().parent().children().get(2).innerText;
  	 //查看
  	 $("#TS_XMGL_look"+pkCode).unbind("click").bind("click", function(){
+ 		d.empty();
 // 	    _viewer._openCardView(UIConst.ACT_CARD_MODIFY,pkCode,"",true);
  		openMyCard(pkCode,true);
  	 });
  	 //编辑
  	 $("#TS_XMGL_edit"+pkCode).unbind("click").bind("click", function(){
+ 		d.empty();
 // 	    rowEdit(pkCode,_viewer,[1000,500],[200,100]);
  		openMyCard(pkCode);
  	 });
  	 //复制
  	 $("#TS_XMGL_copy"+pkCode).unbind("click").bind("click", function(){
+ 		d.empty();
  	    param = {};
  	    param["pkCodes"] = pkCode;
  	    FireFly.doAct(_viewer.servId, "copy", param);
@@ -84,6 +72,7 @@ jQuery("td[icode='buttons'] span").unbind("mouseover").bind("mouseover", functio
  	 });
  	 //设置
  	 $("#TS_XMGL_set"+pkCode).unbind("click").bind("click", function(){
+ 		d.empty();
 	 	var ext = " and XM_ID = '" + pkCode + "'";
 	 	//window.location.href ="stdListView.jsp?frameId=TS_XMGL_SZ-tabFrame&sId=TS_XMGL_SZ&paramsFlag=false&title=项目管理设置&XM_ID="+pkCode+"&extWhere="+ext;
 	   var url =   "stdListView.jsp?frameId=TS_XMGL_SZ-tabFrame&sId=TS_XMGL_SZ&paramsFlag=true&title=项目管理设置&XM_ID="
@@ -101,14 +90,49 @@ jQuery("td[icode='buttons'] span").unbind("mouseover").bind("mouseover", functio
  	 });
  	 //删除
  	 $("#TS_XMGL_delete"+pkCode).unbind("click").bind("click", function(){
+ 		d.empty();
  		rowDelete(pkCode,_viewer);
  	 });
+ 	 
+ 	 d.empty();
+	 var tdTag =this;
+	 var kk = $(this).parent().parent().get(0).clientWidth;//操作td的宽度
+     var top = $(this).parent().parent().get(0).offsetTop; 
+ 	
+ 	 
+ 	 var btn = $("#div_"+pkCode);
+ 	
+ 	 //btn.css("height":tdTag.height);
+ 	 
+ 	 d.append(btn.clone(true).css("display",""));
+ 	 
+// 	var winWidth=windows.innerWidth;if (windows.innerWidth) {
+//        winWidth = windows.innerWidth; 
+//    }else if ((document.body) && (document.body.clientWidth)) {
+//        winWidth = document.body.clientWidth; 
+//}
+
+ 	 dTag.style.display="block";
+ 	 dTag.style.position="fixed";
+ 	 var doc=document.body.clientWidth;
+ 	 
+ 	 dTag.style.left= document.body.clientWidth-2.5*kk+"px"; 
+ 	 dTag.style.top=top+71+"px";
+ 	 p=[dTag.offsetLeft,dTag.offsetTop,dTag.clientWidth,dTag.clientHeight];
+ 	 var spanDiv= $(this).parent().get(0);
+ 	 spanLoc=[spanDiv.offsetLeft,spanDiv.offsetTop,spanDiv.clientWidth,spanDiv.clientHeight];
+ 	 d.focus();
   });
 }
 	
+//jQuery("td[icode='buttons'] ").unbind("mouseout").bind("mouseout", function(){
+//	var tr= $(this).parent().get(0);
+//	
+//});
+
 $("#d").mouseout(function(e){
 	if(p!=null && p.length>0){
-		e=e||window.event;
+		e=e||event;//window.event
 		var x= e.clientX,y=e.clientY;
 		if((x>p[0] && x<p[0]+p[2] && y>p[1] && y<p[1]+p[3]) ){
 			dTag.style.display="block";
@@ -119,8 +143,10 @@ $("#d").mouseout(function(e){
 //			console.log("d:"+p[0]+"   "+p[1]+"   "+p[2]+"   "+p[3]);
 //			 console.log(x+"   "+y);
 			p=null;
+			d.empty();
 		}
 	}
+	
 });
 
 //列表操作按钮 弹dialog
@@ -135,6 +161,7 @@ function openMyCard(dataId,readOnly,showTab){
     }
     var cardView = new rh.vi.cardView(temp);
     cardView.show();
+    d.empty();
 }
 
 /**
