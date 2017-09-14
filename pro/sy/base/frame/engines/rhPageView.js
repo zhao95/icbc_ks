a8355836c7e4ecae4fbc2222f4131d98c9a9f9c0
刘远超 <565767474@qq.com>
2017-09-14 19:11:17 +0800

    隐藏掉皮肤菜单和全文检索搜索框

--- pro/sy/base/frame/engines/rhPageView.js
+++ pro/sy/base/frame/engines/rhPageView.js
@@ -127,7 +127,7 @@ rh.vi.pageView.prototype._bldMenu = function() {
 //			var expandBar = jQuery("<a href='javascript:void(0);' title='收起边栏' class='leftMenu-close'></a>").appendTo(configCon);
 //			var configBar = jQuery("<a href='javascript:void(0);' title='风格配置' class='leftMenu-config'></a>").appendTo(configCon);
 			var expandBar = jQuery("<a href='javascript:void(0);' title='"+Language.transStatic("rhPageView_string1")+"' class='leftMenu-close'></a>").appendTo(configCon);
-			var configBar = jQuery("<a href='javascript:void(0);' title='"+Language.transStatic("rhPageView_string2")+"' class='leftMenu-config'></a>").appendTo(configCon);
+			var configBar = jQuery("<a href='javascript:void(0);' title='"+Language.transStatic("rhPageView_string2")+"' class='leftMenu-config'></a>");//.appendTo(configCon);
 			
 			var toggled = true; 
 			jQuery("ul[aria-expanded='true']").addClass("leftMenu-ul-show");
@@ -435,7 +435,7 @@ rh.vi.pageView.prototype._bldBanner = function() {
 				Tab.open(opts);
 			}
 		});
-		seaCon.appendTo(jQuery("#banner"));	
+//		seaCon.appendTo(jQuery("#banner"));	
 	}
 	//个人、兼岗、委托区
 	var perCon = jQuery("<div></div>").addClass("rh-head-per");
