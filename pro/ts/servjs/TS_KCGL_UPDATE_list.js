ae204421148358101f3329315bc10f76264e627e
刘远超 <565767474@qq.com>
2017-09-19 20:34:57 +0800

    考场按钮

--- pro/ts/servjs/TS_KCGL_UPDATE_list.js
+++ pro/ts/servjs/TS_KCGL_UPDATE_list.js
@@ -42,12 +42,51 @@ $("#TS_KCGL_UPDATE-delete").unbind("click").bind("click",function(){
 
 $("#TS_KCGL_UPDATE .rhGrid").find("tr").each(function(index, item) {
 	if(index != 0){
+		var dataId = item.id;
 		var commit = $(item).find("td[icode='KC_COMMIT']").text();
 		var agree = $(item).find("td[icode='UPDATE_AGREE']").text();
 		if(commit == '1' && agree == '0'){
 			$(item).css("color","blue");
 		}
+		
+		$(item).find("td[icode='view']").append(
+			'<a class="rhGrid-td-rowBtnObj rh-icon" operCode="optDeleteBtn" rowpk="'+dataId+'"><span class="rh-icon-inner">删除</span><span class="rh-icon-img btn-delete"></span></a>'
+		);
+		bindCard();
 	}
 });
-
+function bindCard(){
+	jQuery("td [operCode='optDeleteBtn']").unbind("click").bind("click", function(){
+		var pkCode = jQuery(this).attr("rowpk");
+		rowDelete(pkCode,_viewer);
+	});
+}
 $("#TS_KCGL_UPDATE .rhGrid").find("th[icode='view']").html("操作");
+
+_viewer.grid.getBtn("view").unbind("click").bind("click",function() {
+	var pkCode = jQuery(this).attr("rowpk");//获取主键信息
+	openMyCard(pkCode);
+});
+
+//列表操作按钮 弹dialog
+function openMyCard(dataId,readOnly,showTab){
+	var height = jQuery(window).height()-200;
+	var width = jQuery(window).width()-200;
+	
+	var temp = {"act":UIConst.ACT_CARD_MODIFY,"sId":_viewer.servId,"parHandler":_viewer,"widHeiArray":[width,height],"xyArray":[100,100]};
+    temp[UIConst.PK_KEY] = dataId;
+    if(readOnly != ""){
+    	temp["readOnly"] = readOnly;
+    }
+    if(showTab != ""){
+    	temp["showTab"] = showTab;
+    }
+    var cardView = new rh.vi.cardView(temp);
+    cardView.show();
+}
+/*
+ * 删除前方法执行
+ */
+rh.vi.listView.prototype.beforeDelete = function(pkArray) {
+	showVerify(pkArray,_viewer);
+};
--- pro/ts/servjs/TS_KCGL_ZWDYB_list.js
+++ pro/ts/servjs/TS_KCGL_ZWDYB_list.js
@@ -1,7 +1,7 @@
 var _viewer = this;
 
-$(".rhGrid").find("tr").unbind("dblclick"); 
-$(".rhGrid").find("th[icode='del']").html("操作");
+$("#TS_KCGL_ZWDYB .rhGrid").find("tr").unbind("dblclick"); 
+$("#TS_KCGL_ZWDYB .rhGrid").find("th[icode='del']").html("操作");
 
 //删除单行数据
 _viewer.grid.getBtn("del").unbind("click").bind("click",function() {
@@ -15,3 +15,36 @@ _viewer.grid.getBtn("del").unbind("click").bind("click",function() {
 rh.vi.listView.prototype.beforeDelete = function(pkArray) {
 	showVerify(pkArray,_viewer);
 };
+$("#TS_KCGL_ZWDYB .rhGrid").find("tr").each(function(index, item) {
+	if(index != 0){
+		var dataId = item.id;
+		$(item).find("td[icode='del']").prepend(
+				'<a class="rhGrid-td-rowBtnObj rh-icon" operCode="optEditBtn" rowpk="'+dataId+'"><span class="rh-icon-inner">编辑</span><span class="rh-icon-img btn-edit"></span></a>'
+		);
+		bindCard();
+	}
+});	
+
+function bindCard(){
+	jQuery("td [operCode='optEditBtn']").unbind("click").bind("click", function(){
+		var pkCode = jQuery(this).attr("rowpk");
+		openMyCard(pkCode);
+	});
+}
+
+//列表操作按钮 弹dialog
+function openMyCard(dataId,readOnly,showTab){
+	var height = jQuery(window).height()-200;
+	var width = jQuery(window).width()-200;
+	
+	var temp = {"act":UIConst.ACT_CARD_MODIFY,"sId":_viewer.servId,"parHandler":_viewer,"widHeiArray":[width,height],"xyArray":[100,100]};
+    temp[UIConst.PK_KEY] = dataId;
+    if(readOnly != ""){
+    	temp["readOnly"] = readOnly;
+    }
+    if(showTab != ""){
+    	temp["showTab"] = showTab;
+    }
+    var cardView = new rh.vi.cardView(temp);
+    cardView.show();
+}
