ee5bcbab374244bc03206e0f6717f541deabf75c
刘远超 <565767474@qq.com>
2017-09-20 21:38:49 +0800

    考场 保存出现双列表bug

--- pro/ts/servjs/TS_KCGL_UPDATE_card.js
+++ pro/ts/servjs/TS_KCGL_UPDATE_card.js
@@ -7,13 +7,13 @@ if (_viewer._actVar == "cardAdd") {
 	}
 	var msg = "新建一条申请";
 	if (confirm(msg) == true) {
-		_viewer._saveForm();
+		_viewer.btns[UIConst.ACT_SAVE].click();
 	} else {
 		_viewer.backA.mousedown();
 	}
 }
 
-_viewer.getBtn("save").css("right", "500px");
+//_viewer.getBtn("save").css("right", "500px");
 _viewer.getBtn("commit").unbind("click").bind("click", function(event) {
 	if (_viewer.getItem("KC_COMMIT").getValue() == 1) {
 		alert("申请单已提交过，无需重复提交");
