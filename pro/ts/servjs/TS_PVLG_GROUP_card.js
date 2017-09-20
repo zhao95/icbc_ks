cac52e86a22ef7d90d94386c35e00dff63b9c461
lianxinshuai <934815805@qq.com>
2017-09-20 21:32:47 +0800

    TIME

--- pro/ts/servjs/TS_PVLG_GROUP_card.js
+++ pro/ts/servjs/TS_PVLG_GROUP_card.js
@@ -2,12 +2,14 @@ _viewer=this;
 //时间表单校验
 _viewer.getItem("G_DEAD_BEGIN").obj.unbind("click").bind("click", function() {
 	WdatePicker({
+		dateFmt: 'yyyy-MM-dd HH:mm:ss';
 		maxDate : "#F{$dp.$D('" + _viewer.servId + "-G_DEAD_END')}"
 	});
 });
 _viewer.getItem("G_DEAD_END").obj.unbind("click").bind("click", function() {
 
 	WdatePicker({
+		dateFmt: 'yyyy-MM-dd HH:mm:ss';
 		minDate : "#F{$dp.$D('" + _viewer.servId + "-G_DEAD_BEGIN')}"
 	});
 });
