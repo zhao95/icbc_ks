9c5527cc0b7c013b3765d564aecbd37b89ee698f
刘远超 <565767474@qq.com>
2017-10-18 15:22:47 +0800

    流程节点 添加没有流程ID的问题修复

--- pro/ts/servjs/TS_WFS_QJKLC_card.js
+++ pro/ts/servjs/TS_WFS_QJKLC_card.js
@@ -2,8 +2,12 @@ var _viewer = this;
 //获取wfs_ID,并保存
 if(_viewer.opts.act == "cardAdd"){
 	var WFS_ID = _viewer.opts.WFS_ID;
+	var NODE_ID = _viewer.opts.WFS_ID;
 	if(typeof(WFS_ID)!="undefined"){ 
 		_viewer.getItem("WFS_ID").setValue(WFS_ID);
+	}
+	if(typeof(NODE_ID)!="undefined"){ 
+		_viewer.getItem("NODE_ID").setValue(NODE_ID);
 	}
 }
 
