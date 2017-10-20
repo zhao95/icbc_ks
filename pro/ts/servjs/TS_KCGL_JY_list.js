b7cf796431f4c5472185b1fd06077e5642ebe147
刘远超 <565767474@qq.com>
2017-10-20 10:07:43 +0800

    考场禁用 增加 开放考场功能

--- pro/ts/servjs/TS_KCGL_JY_list.js
+++ pro/ts/servjs/TS_KCGL_JY_list.js
@@ -6,8 +6,9 @@ $("#TS_KCGL_JY .rhGrid").find("tr").each(function(index, item) {
 		var dataId = item.id;		
 		$(item).find("td[icode='BUTTONS']").append(
 				'<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_KCGL_JY-update" actcode="update" rowpk="'+dataId+'"><span class="rh-icon-inner">编辑</span><span class="rh-icon-img btn-edit"></span></a>'+
-				'<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_KCGL_JY-stop" actcode="stop" rowpk="'+dataId+'"><span class="rh-icon-inner">禁用</span><span class="rh-icon-img btn-edit"></span></a>'
-				);
+				'<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_KCGL_JY-stop" actcode="stop" rowpk="'+dataId+'"><span class="rh-icon-inner">禁用</span><span class="rh-icon-img btn-edit"></span></a>'+
+				'<a class="rhGrid-td-rowBtnObj rh-icon" id="TS_KCGL_JY-open" actcode="open" rowpk="'+dataId+'"><span class="rh-icon-inner">解禁</span><span class="rh-icon-img btn-edit"></span></a>'
+		);
 		// 为每个按钮绑定卡片
 		bindCard();
 	}
@@ -33,6 +34,25 @@ function bindCard(){
 		param["_PK_"] = pkCode;
 		param["KC_STATE"] = 6;
 		if (confirm("确定禁用此考场？") == true) {
+			FireFly.doAct(_viewer.servId, "save", param, true, false,function(data) {
+				if (true) {
+					_viewer.refreshGrid();
+				}
+			});
+		}
+	});
+	//放开考场
+	jQuery("td [id='TS_KCGL_JY-open']").unbind("click").bind("click",function() {
+		var pkCode = jQuery(this).attr("rowpk");
+		var state = jQuery(this).parent().parent().find("td[icode='KC_STATE']").text();
+		if(state == 5){
+			_viewer.listBarTipError("当前考场未被禁用！");
+			return;
+		}
+		var param = {};
+		param["_PK_"] = pkCode;
+		param["KC_STATE"] = 5;
+		if (confirm("确定开放此考场？") == true) {
 			FireFly.doAct(_viewer.servId, "save", param, true, false,function(data) {
 				if (true) {
 					_viewer.refreshGrid();
