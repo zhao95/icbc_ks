7421cbdf10609bd93e915c8465aba0c99bd7f81d
lianxinshuai <934815805@qq.com>
2017-10-25 12:16:00 +0800

    时间

--- pro/ts/servjs/TS_XMGL_card.js
+++ pro/ts/servjs/TS_XMGL_card.js
@@ -1,17 +1,51 @@
 /** 服务卡片使用的js方法定义：开始fromTable */
 var _viewer = this;
-// 针对时间的校验
-_viewer.getItem("XM_START").obj.unbind("click").bind("click", function() {
-	WdatePicker({
-		maxDate : "#F{$dp.$D('" + _viewer.servId + "-XM_END')}"
-	});
-});
-_viewer.getItem("XM_END").obj.unbind("click").bind("click", function() {
+//针对项目开始时间的校验与互斥
+_viewer.beforeSave = function() {
+	var xmStart=_viewer.getItem("XM_START").getValue();//项目开始时间
+	var xmEnd=_viewer.getItem("XM_END").getValue();//项目截至时间
+	var xmKsStartData=_viewer.getItem("XM_KSSTARTDATA").getValue();//考试开始时间
+	var xmKsEndData=_viewer.getItem("XM_KSENDDATA").getValue();//考试截至时间
+	var xmStarts = xmStart.substring(0, 10).split('-');
+    var xmEnds = xmEnd.substring(0, 10).split('-');
+    var xmKsStartDatas = xmKsStartData.substring(0, 10).split('-');
+    var xmKsEndDatas = xmKsEndData.substring(0, 10).split('-');
+    xmStart = xmStarts[1] + '-' + xmStarts[2] + '-' + xmStarts[0] + ' ' + xmStart.substring(10, 19);
+    xmEnd = xmEnds[1] + '-' + xmEnds[2] + '-' + xmEnds[0] + ' ' + xmEnd.substring(10, 19);
+    xmKsStartData=xmKsStartDatas[1] + '-' + xmKsStartDatas[2] + '-' + xmKsStartDatas[0] + ' ' + xmKsStartData.substring(10, 19);
+    xmKsEndDatas= xmKsEndDatas[1] + '-' + xmKsEndDatas[2] + '-' + xmKsEndDatas[0] + ' ' + xmKsEndData.substring(10, 19);
+    //项目开始时间和考试考试时间互斥
+    var ksStartXmStart = (Date.parse(xmKsStartData) - Date.parse(xmStart)) / 3600 / 1000;
+    if(ksStartXmStart< 0||ksStartXmStart== 0||isNaN(ksStartXmStart)){
+ 		$("#TS_XMGL-XM_START").addClass("blankError").addClass("errorbox");
+ 		$("#TS_XMGL-XM_KSSTARTDATA").addClass("blankError").addClass("errorbox");
+		return false;
+ 	}
+    //考试开始时间和考试结束时间互斥
+    var ksEndKsStart = (Date.parse(xmKsEndDatas) - Date.parse(xmKsStartData)) / 3600 / 1000;
+    if(ksEndKsStart< 0||ksEndKsStart== 0||isNaN(ksEndKsStart)){
+ 		$("#TS_XMGL-XM_KSSTARTDATA").addClass("blankError").addClass("errorbox");
+ 		$("#TS_XMGL-XM_KSENDDATA").addClass("blankError").addClass("errorbox");
+		return false;
+ 	}
+    //考试结束时间与项目结束时间互斥
+    var xmEndKsEnd = (Date.parse(xmEnd) - Date.parse(xmKsEndDatas)) / 3600 / 1000;
+     if(xmEndKsEnd < 0||xmEndKsEnd == 0||isNaN(xmEndKsEnd)){
+ 		$("#TS_XMGL-XM_END").addClass("blankError").addClass("errorbox");
+ 		$("#TS_XMGL-XM_KSENDDATA").addClass("blankError").addClass("errorbox");
+		return false;
+ 	}
+};
+//修改input时间样式
+var xmStart=_viewer.getItem("XM_START");
+var xmEnd=_viewer.getItem("XM_END");
+var xmKsStartData=_viewer.getItem("XM_KSSTARTDATA");
+var xmKsEndData=_viewer.getItem("XM_KSENDDATA");
+$('#'+xmStart._opts.id+"_div").css('min-height','32px');
+$('#'+xmEnd._opts.id+"_div").css('min-height','32px');
+$('#'+xmKsStartData._opts.id+"_div").css('min-height','32px');
+$('#'+xmKsEndData._opts.id+"_div").css('min-height','32px');
 
-	WdatePicker({
-		minDate : "#F{$dp.$D('" + _viewer.servId + "-XM_START')}"
-	});
-});
 // 下一步按钮
 // 1把数据保存到数据库
 _viewer.getBtn("nextbtn").unbind("click").bind("click",function(event) {
