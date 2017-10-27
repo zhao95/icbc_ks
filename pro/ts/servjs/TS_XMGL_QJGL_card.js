3d63a41982c414a32a4c9e09c6d97d44cedabc37
lianxinshuai <934815805@qq.com>
2017-10-27 11:19:06 +0800

    修改

--- pro/ts/servjs/TS_XMGL_QJGL_card.js
+++ pro/ts/servjs/TS_XMGL_QJGL_card.js
@@ -10,21 +10,23 @@ if(_viewer.opts.act == "cardAdd"){
 		_viewer.getItem("XM_ID").setValue(XM_ID);
 	}
 }
-
 //针对开始和结束时间的校验
-_viewer.getItem("QJ_STADATE").obj.unbind("click").bind("click", function() {
-	    WdatePicker({
-		    dateFmt: 'yyyy-MM-dd HH:mm:ss',
-	        maxDate: "#F{$dp.$D('" + _viewer.servId + "-QJ_ENDDATE')}"
-	    });
-	});
-_viewer.getItem("QJ_ENDDATE").obj.unbind("click").bind("click", function() {
-	    WdatePicker({
-		    dateFmt: 'yyyy-MM-dd HH:mm:ss',
-	        minDate: "#F{$dp.$D('" + _viewer.servId + "-QJ_STADATE')}"
-	    });
-	});
- 
+_viewer.beforeSave = function() {
+	var beginTime=_viewer.getItem("QJ_STADATE").getValue();
+	var endTime=_viewer.getItem("QJ_ENDDATE").getValue();
+    var beginTimes = beginTime.substring(0, 10).split('-');
+    var endTimes = endTime.substring(0, 10).split('-');
+     beginTime = beginTimes[1] + '-' + beginTimes[2] + '-' + beginTimes[0] + ' ' + beginTime.substring(10, 19);
+     endTime = endTimes[1] + '-' + endTimes[2] + '-' + endTimes[0] + ' ' + endTime.substring(10, 19);
+    var a = (Date.parse(endTime) - Date.parse(beginTime)) / 3600 / 1000;
+    if(a < 0||a == 0){
+ 		$("#TS_XMGL_QJGL-QJ_STADATE").addClass("blankError").addClass("errorbox");
+ 		$("#TS_XMGL_QJGL-QJ_ENDDATE").addClass("blankError").addClass("errorbox");
+		return false;
+ 	}
+};
+
+
 
 
 
--- pro/ts/servjs/TS_XMGL_YDJK_card.js
+++ pro/ts/servjs/TS_XMGL_YDJK_card.js
@@ -12,20 +12,35 @@ if(_viewer.opts.act == "cardAdd"){
 }
 
 //针对开始和结束时间的校验
-_viewer.getItem("YDJK_STADATE").obj.unbind("click").bind("click", function() {
-	    WdatePicker({
-		 dateFmt: 'yyyy-MM-dd HH:mm:ss',
-	        maxDate : "#F{$dp.$D('" + _viewer.servId + "-YDJK_ENDDATE')}"
-	    });
-	});
-_viewer.getItem("YDJK_ENDDATE").obj.unbind("click").bind("click", function() {
-
-	    WdatePicker({
-		 dateFmt: 'yyyy-MM-dd HH:mm:ss',
-	        minDate : "#F{$dp.$D('" + _viewer.servId + "-YDJK_STADATE')}"
-	    });
-	});
- 
+_viewer.beforeSave = function() {
+	var beginTime=_viewer.getItem("YDJK_STADATE").getValue();
+	var endTime=_viewer.getItem("YDJK_ENDDATE").getValue();
+    var beginTimes = beginTime.substring(0, 10).split('-');
+    var endTimes = endTime.substring(0, 10).split('-');
+     beginTime = beginTimes[1] + '-' + beginTimes[2] + '-' + beginTimes[0] + ' ' + beginTime.substring(10, 19);
+     endTime = endTimes[1] + '-' + endTimes[2] + '-' + endTimes[0] + ' ' + endTime.substring(10, 19);
+    var a = (Date.parse(endTime) - Date.parse(beginTime)) / 3600 / 1000;
+    if(a < 0||a == 0){
+ 		$("#TS_XMGL_YDJK-YDJK_STADATE").addClass("blankError").addClass("errorbox");
+ 		$("#TS_XMGL_YDJK-YDJK_ENDDATE").addClass("blankError").addClass("errorbox");
+		return false;
+ 	}
+};
+////针对开始和结束时间的校验
+//_viewer.getItem("YDJK_STADATE").obj.unbind("click").bind("click", function() {
+//	    WdatePicker({
+//		 dateFmt: 'yyyy-MM-dd HH:mm:ss',
+//	        maxDate : "#F{$dp.$D('" + _viewer.servId + "-YDJK_ENDDATE')}"
+//	    });
+//	});
+//_viewer.getItem("YDJK_ENDDATE").obj.unbind("click").bind("click", function() {
+//
+//	    WdatePicker({
+//		 dateFmt: 'yyyy-MM-dd HH:mm:ss',
+//	        minDate : "#F{$dp.$D('" + _viewer.servId + "-YDJK_STADATE')}"
+//	    });
+//	});
+// 
 
 
 
