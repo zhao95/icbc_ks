2c9a5099ac1f23ac55ba2c16df6ebdea6730adae
朱晋磊 <739901717@qq.com>
2017-11-10 18:39:43 +0800

    考场安排-待安排考生权限

---
+++ src/com/rh/ts/xmgl/KcapDfpksServ.java
@@ -0,0 +1,18 @@
+package com.rh.ts.xmgl;
+
+import com.rh.core.serv.CommonServ;
+import com.rh.core.serv.ParamBean;
+import com.rh.ts.pvlg.PvlgUtils;
+
+public class KcapDfpksServ extends CommonServ {
+
+	// 查询前添加查询条件
+	protected void beforeQuery(ParamBean paramBean) {
+		ParamBean param = new ParamBean();
+		param.set("paramBean", paramBean);
+		param.set("serviceName", paramBean.getServId());
+		param.set("fieldName", "S_ODEPT");
+		PvlgUtils.setOrgPvlgWhere(param);
+	}
+
+}
