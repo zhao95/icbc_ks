d1b557fc15b2ac262ac6b8995918ff4934ce9e3a
lianxinshuai <934815805@qq.com>
2017-11-23 14:34:02 +0800

    大时间段

--- pro/ts/servjs/TS_XMGL_KCAP_DAPCC_CCSJ_card.js
+++ pro/ts/servjs/TS_XMGL_KCAP_DAPCC_CCSJ_card.js
@@ -10,38 +10,38 @@ var _viewer = this;
 //		minDate : "#F{$dp.$D('" + _viewer.servId + "-SJ_START')}"
 //	});
 //});
-//针对开始和结束时间的校验
-//_viewer.beforeSave = function() {
-//	var beginTime=_viewer.getItem("SJ_START").getValue();
-//	var endTime=_viewer.getItem("SJ_END").getValue();
-//    var beginTimes = beginTime.substring(0, 10).split('-');
-//    var endTimes = endTime.substring(0, 10).split('-');
-//     beginTime = beginTimes[1] + '-' + beginTimes[2] + '-' + beginTimes[0] + ' ' + beginTime.substring(10, 19);
-//     endTime = endTimes[1] + '-' + endTimes[2] + '-' + endTimes[0] + ' ' + endTime.substring(10, 19);
-//    var a = (Date.parse(endTime) - Date.parse(beginTime)) / 3600 / 1000;
-//    if(a < 0||a == 0){
-// 		$("#TS_XMGL_KCAP_DAPCC_CCSJ-SJ_START").addClass("blankError").addClass("errorbox");
-// 		$("#TS_XMGL_KCAP_DAPCC_CCSJ-SJ_END").addClass("blankError").addClass("errorbox");
-//		return false;
-// 	}
-//};
-
+针对开始和结束时间的校验
 _viewer.beforeSave = function() {
-var SJCC=_viewer.getItem("SJ_CC").getValue();//场次
-var sjDate=_viewer.getItem("SJ_DATE").getValue();//日期
-var sjKsTime=_viewer.getItem("SJ_KSTIME").getValue();//起始时间
-var sjsc=_viewer.getItem("SJ_SC").getValue();//时长
-var start = sjDate + " " +sjKsTime;//
-var end=addMin(start,sjsc);
+	var beginTime=_viewer.getItem("SJ_START").getValue();
+	var endTime=_viewer.getItem("SJ_END").getValue();
+    var beginTimes = beginTime.substring(0, 10).split('-');
+    var endTimes = endTime.substring(0, 10).split('-');
+     beginTime = beginTimes[1] + '-' + beginTimes[2] + '-' + beginTimes[0] + ' ' + beginTime.substring(10, 19);
+     endTime = endTimes[1] + '-' + endTimes[2] + '-' + endTimes[0] + ' ' + endTime.substring(10, 19);
+    var a = (Date.parse(endTime) - Date.parse(beginTime)) / 3600 / 1000;
+    if(a < 0||a == 0){
+ 		$("#TS_XMGL_KCAP_DAPCC_CCSJ-SJ_START").addClass("blankError").addClass("errorbox");
+ 		$("#TS_XMGL_KCAP_DAPCC_CCSJ-SJ_END").addClass("blankError").addClass("errorbox");
+		return false;
+ 	}
+};
 
-_viewer.getItem("SJ_START").setValue(start);
-_viewer.getItem("SJ_END").setValue(end);
-_viewer.getParHandler().refresh();
-//_viewer.backA.mousedown();
-}
-_viewer.afterSave = function() {
-_viewer.getParHandler().refresh();
-}
+//_viewer.beforeSave = function() {
+//var SJCC=_viewer.getItem("SJ_CC").getValue();//场次
+//var sjDate=_viewer.getItem("SJ_DATE").getValue();//日期
+//var sjKsTime=_viewer.getItem("SJ_KSTIME").getValue();//起始时间
+//var sjsc=_viewer.getItem("SJ_SC").getValue();//时长
+//var start = sjDate + " " +sjKsTime;//
+//var end=addMin(start,sjsc);
+//
+//_viewer.getItem("SJ_START").setValue(start);
+//_viewer.getItem("SJ_END").setValue(end);
+////_viewer.getParHandler().refresh();
+////_viewer.backA.mousedown();
+//}
+//_viewer.afterSave = function() {
+//_viewer.getParHandler().refresh();
+//}
 function addMin(a,b){
 	b = parseInt(b);
 	var atime = a.replace(/-/g, "/");
@@ -63,12 +63,12 @@ if(_viewer.getItem("SJ_ADDTYPE").getValue() == 0){
 	$("#TS_XMGL_KCAP_DAPCC_CCSJ-SJ_END").attr("disabled","disabled"); 
 }
 
-//_viewer.getItem("SJ_START").change(function(){
-//	checkShow();
-//});
-//_viewer.getItem("SJ_END").change(function(){
-//	checkShow();
-//});
+_viewer.getItem("SJ_START").change(function(){
+	checkShow();
+});
+_viewer.getItem("SJ_END").change(function(){
+	checkShow();
+});
 
 function checkShow(){
 	var start = _viewer.getItem("SJ_START").getValue();
