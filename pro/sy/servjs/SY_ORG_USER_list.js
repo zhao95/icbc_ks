1f1eb6f063a5dd50153cfa753374469156dafc4f
刘远超 <565767474@qq.com>
2017-11-28 21:27:04 +0800

    机构人员列表添加行按钮

--- pro/sy/servjs/SY_ORG_USER_list.js
+++ pro/sy/servjs/SY_ORG_USER_list.js
@@ -5,3 +5,27 @@ res.unbind("click").bind("click",function() {
 	var options = {"url":"SY_ORG_DEPT.list.do?extWhere=aaaa","tTitle":"test","menuFlag":3};
 	Tab.open(options);
 });*/
+$(".rhGrid").find("tr").unbind("dblclick"); 
+$(".rhGrid").find("th[icode='editBtn']").html("操作");
+
+_viewer.grid.getBtn("editBtn").unbind("click").bind("click",function() {
+	var pk = jQuery(this).attr("rowpk");//获取主键信息
+	openMyCard(pk);
+});
+
+//列表操作按钮 弹dialog
+function openMyCard(dataId,readOnly,showTab){
+	var height = jQuery(window).height()-200;
+	var width = jQuery(window).width()-200;
+	
+	var temp = {"act":UIConst.ACT_CARD_MODIFY,"sId":_viewer.servId,"parHandler":_viewer,"widHeiArray":[width,height],"xyArray":[100,100]};
+    temp[UIConst.PK_KEY] = dataId;
+    if(readOnly != ""){
+    	temp["readOnly"] = readOnly;
+    }
+    if(showTab != ""){
+    	temp["showTab"] = showTab;
+    }
+    var cardView = new rh.vi.cardView(temp);
+    cardView.show();
+}
