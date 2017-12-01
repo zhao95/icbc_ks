1ccb8d81d9f82e4dcddd408f89f2ec9d02d0d8f4
刘远超 <565767474@qq.com>
2017-12-01 16:20:11 +0800

    考场审核 优化

--- pro/ts/servjs/TS_KCGL_SH_card.js
+++ pro/ts/servjs/TS_KCGL_SH_card.js
@@ -23,11 +23,13 @@ var kcState2 = _viewer.getItem("KC_STATE2").getValue();
 //状态 0:新增未保存1:无效(待审核) 2:无效(审核未通过) 3:无效(审核中) 4:无效(扣分超过上限)5:有效
 _viewer.getBtn("yesBtn").unbind("click").bind("click", function(event) {
 	var param = {"_PK_":pkCode};
-	if(roleOrgPvlg == "3"){
-		param["KC_STATE2"] = 1;
-	}else{
+	
+	if(roleOrgPvlg.indexOf('2') > -1){
 		param["KC_STATE"] = 5;
+	}else{
+		param["KC_STATE2"] = 1;
 	}
+	
 	FireFly.doAct(_viewer.servId, "save", param, true,false,function(data){
 		alert("审核成功");
 		_viewer.refresh();
