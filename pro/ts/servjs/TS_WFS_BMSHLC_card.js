98dfc8335c312d6fee43052e11caf9a786f7f133
刘远超 <565767474@qq.com>
2017-12-05 14:01:20 +0800

    单个页面控制关闭按钮

--- pro/ts/servjs/TS_WFS_BMSHLC_card.js
+++ pro/ts/servjs/TS_WFS_BMSHLC_card.js
@@ -12,7 +12,32 @@ if(_viewer.opts.act == "cardAdd"){
 }
 
 
-//$(".ui-icon-closethick").unbind("click").bind("click",function(){debugger;
-//	var   a=$("#TS_WFS_NODE_APPLY-save").click();
-//	console.log(a);
-//});
+if ((_viewer._actVar == UIConst.ACT_CARD_ADD)) {//添加
+	_viewer.backA.unbind("mousedown").bind("mousedown",function(){
+		var shr = _viewer.getItem("BMSHLC_SHR").getValue();
+		var deptCode = _viewer.getItem("DEPT_CODE").getValue();
+		if(shr != "" || deptCode != ""){
+			var confirmDel=confirm("新增数据，是否保存？");
+			if (confirmDel == true){
+				if (_viewer.btns[UIConst.ACT_SAVE]) {
+					_viewer.btns[UIConst.ACT_SAVE].click();
+				}
+			}else{
+				jQuery("#" + _viewer.dialogId).dialog("close");
+			}
+		}
+	});
+}
+//if ((_self._actVar == UIConst.ACT_CARD_MODIFY) && (_self.beforeSaveCheck == true)) {//修改
+//	   if (jQuery.isEmptyObject(_self.getChangeData())) {
+//	   } else {
+////		   var confirmDel=confirm("数据有修改，是否保存？");
+//		   var confirmDel=confirm(Language.transStatic("rhCardView_string1"));
+//		   if (confirmDel == true){
+//			   if (_self.btns[UIConst.ACT_SAVE]) {
+//				   _self.btns[UIConst.ACT_SAVE].click();
+//				   return false;
+//			   }
+//		   }
+//	   }
+//}
