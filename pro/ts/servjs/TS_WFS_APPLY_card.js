f2d0f02920b0948c7417ae316e3181ce689bac42
lianxinshuai <934815805@qq.com>
2017-12-11 09:26:07 +0800

    流程增加管理审核人

--- pro/ts/servjs/TS_WFS_APPLY_card.js
+++ pro/ts/servjs/TS_WFS_APPLY_card.js
@@ -1,11 +1,20 @@
 var _viewer = this;
 var flowServ = _viewer.getItem("WFS_SERVID").getValue();
-if(flowServ == 1){
+if(flowServ == 1 ){
+	_viewer.getItem("WFS_TYPE").show();
+}
+if(flowServ == 3){
 	_viewer.getItem("WFS_TYPE").show();
 }
 _viewer.getItem("WFS_SERVID").change(function(){
 	var flowSerTmp = _viewer.getItem("WFS_SERVID").getValue(); 
 	if(flowSerTmp == 1){
+		_viewer.getItem("WFS_TYPE").show();
+	}else{
+		_viewer.getItem("WFS_TYPE").hide();
+	}
+	
+	if(flowSerTmp == 3){
 		_viewer.getItem("WFS_TYPE").show();
 	}else{
 		_viewer.getItem("WFS_TYPE").hide();
