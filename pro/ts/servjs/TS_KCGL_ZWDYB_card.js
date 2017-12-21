995c7cdc1954f7b1b7def85a81452fc219a2a433
刘远超 <565767474@qq.com>
2017-12-21 10:10:01 +0800

    考场系统座位号对应  1.增加保存添加下一条按钮，且对按钮增加了表单验证  2.增加校验 系统座位号同一考场不能重复

--- pro/ts/servjs/TS_KCGL_ZWDYB_card.js
+++ pro/ts/servjs/TS_KCGL_ZWDYB_card.js
@@ -11,3 +11,29 @@ if(_viewer.opts.act == "cardAdd"){
 if (_viewer.opts.readOnly) {
 	_viewer.readCard();
 	}
+
+_viewer.getBtn("saveAndAdd").click(function(){
+	if(saveCheck()){
+		_viewer._saveForm();
+		setTimeout(function(){ 
+			jQuery("#" + _viewer.dialogId).dialog("close");
+			$("#TS_KCGL_ZWDYB-add").click();
+		}, 200);
+	} else{
+		_viewer.cardBarTipError("校验未通过");
+	}
+});
+
+function saveCheck(){
+	var ZW_ZWH_XT = _viewer.getItem("ZW_ZWH_XT").getValue();
+	var ZW_ZWH_SJ = _viewer.getItem("ZW_ZWH_SJ").getValue();
+	if(ZW_ZWH_XT == "" || ZW_ZWH_SJ == "") return false;
+	var reg = new RegExp("^([0-9]{1,3})\-([0-9]{1,3})$");
+	if(!reg.test(ZW_ZWH_XT)){
+		return false;
+    }
+	var kcId = _viewer.getItem("KC_ID").getValue();
+	var num = FireFly.doAct("TS_KCGL_ZWDYB","count",{"_WHERE_":"and kc_id = '"+kcId+"' and ZW_ZWH_XT = '"+ZW_ZWH_XT+"'"},true,false)._DATA_;
+	if(num > 0) return false;
+	return true;
+}
