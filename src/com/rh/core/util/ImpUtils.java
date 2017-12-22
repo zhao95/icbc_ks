6e9ad4faaa3754f03618006477cdb27e8c8c4051
shiyun <shiyun@ruaho.com>
2017-12-22 18:01:10 +0800

    导入模板

--- src/com/rh/core/util/ImpUtils.java
+++ src/com/rh/core/util/ImpUtils.java
@@ -7,13 +7,17 @@ import com.rh.core.comm.file.TempFile;
 import com.rh.core.org.UserBean;
 import com.rh.core.org.mgr.UserMgr;
 import com.rh.core.serv.OutBean;
+import com.rh.core.serv.ParamBean;
 import com.rh.core.serv.ServDao;
+import com.rh.core.serv.ServMgr;
+
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.format.Alignment;
 import jxl.format.Colour;
 import jxl.write.*;
+
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
@@ -54,88 +58,31 @@ public class ImpUtils {
      * @return errorFileId
      * @throws WriteException 写入失败
      */
-    public static String saveErrorAndReturnErrorFile(String fileId, List<Bean> rowBeanList) throws WriteException {
+    public static void saveErrorAndReturnErrorFile(int i ,WritableSheet wSheet,Sheet sheet, List<Bean> rowBeanList) throws WriteException {
         OutBean outBean = new OutBean();
-        Bean fileBean = FileMgr.getFile(fileId);
-        if (fileBean != null/* && fileBean.getStr("FILE_MTYPE").equals("application/vnd.ms-excel")*/) { //只支持excel类型
-            Workbook book = null;
-            InputStream in = null;
-            OutputStream os = null;
-            TempFile tempFile = null;
-            boolean isOk = true;
-
-            try {
-                in = FileMgr.download(fileBean);
-                log.debug("imp---->open file ");
-                //打开文件
-                try {
-                    book = Workbook.getWorkbook(in);
-                } catch (Exception e) {
-                    log.error(e.getMessage(), e);
-                    throw new RuntimeException("请使用excel 2003导入!");
-                }
-
-                tempFile = new TempFile(TempFile.Storage.SMART);
-                os = tempFile.getOutputStream();
-
-                log.debug("imp---->open file 1");
-                WritableWorkbook wbook = Workbook.createWorkbook(os, book);
-                WritableSheet wSheet = wbook.getSheet(0);
-
-                //取得第一个sheet
-                Sheet sheet = book.getSheet(0);
+        //获取下载excel  sheet页
 //                final int titleRowNum = getImpTitleRowNum();
                 //取得第一行数据，这里非常重要，只要遇到空的cell，则忽略且不再往后处理
 //                int columns = sheet.getColumns();
 //                Cell[] titleCell = sheet.getRow(titleRowNum);
                 int cols = sheet.getColumns();
-                for (int i = 0; i < rowBeanList.size(); i++) {
-                    Bean bean = rowBeanList.get(i);
+                for (int j = 0; j < rowBeanList.size(); j++) {
+                    Bean bean = rowBeanList.get(j);
                     WritableFont font = new WritableFont(WritableFont.COURIER);
                     font.setColour(Colour.GREEN);
                     WritableCellFormat format = new WritableCellFormat(font);
                     format.setAlignment(Alignment.CENTRE);
                     if (StringUtils.isEmpty(bean.getStr(ERROR_NAME))) {//没有出现错误信息
-//                        Label label = new Label(cols, i, "成功", format);
-//                        wSheet.addCell(label);
+                       Label label = new Label(cols, i+j-rowBeanList.size()+1, "成功", format);
+                       wSheet.addCell(label);
                     } else {
                         font.setColour(Colour.RED);
-                        Label label = new Label(cols, i, "失败", format);
+                        Label label = new Label(cols, i+j-rowBeanList.size()+1, "失败", format);
                         wSheet.addCell(label);
-                        isOk = false;
-                        Label label2 = new Label(cols + 1, i, bean.getStr(ERROR_NAME), format);
+                        Label label2 = new Label(cols + 1, i+j-rowBeanList.size()+1, bean.getStr(ERROR_NAME), format);
                         wSheet.addCell(label2);
                     }
                 }
-
-                log.debug("imp---->close File");
-                //关闭文件
-                wbook.write();
-                wbook.close();
-
-                fileBean.set("FILE_NAME", fileBean.getStr("DIS_NAME") + "-导入结果.xls");
-                Bean newFileBean = saveTempFile(fileBean, tempFile);
-
-                if (newFileBean != null) {
-                    outBean.set("FILE_ID", newFileBean.getId());
-                }
-            } catch (Exception e) {
-                outBean.setError("导入失败:文件格式错误，" + e.getMessage());
-                isOk = false;
-                log.error(e.getMessage(), e);
-            } finally {
-                IOUtils.closeQuietly(in);
-                IOUtils.closeQuietly(os);
-                FileMgr.deleteFile(fileId);
-            }
-
-            if (!isOk) {
-//            	outBean.setError("导入失败，请查看文件。");
-            } else {
-                tempFile.destroy();
-            }
-        }
-        return outBean.getStr("FILE_ID");
     }
 
     /**
@@ -189,7 +136,7 @@ public class ImpUtils {
     }
 
 
-    /**
+   /* *//**
      * 通过fileId获取第一个sheet的内容
      *
      * @param fileId      文件id
@@ -197,7 +144,7 @@ public class ImpUtils {
      *                    例：["USER_CODE","USER_NAME"] 第一列的值key为USER_CODE，第二列的值key为USER_NAME
      * @return outBean  rowBeans(List<Bean>)
      * @throws TipException 文件不存在，请重试 / Excel文件解析错误，请校验！
-     */
+     *//*
     public static List<Bean> getDataFromXls(String fileId, List<String> colCodeList) {
         List<Bean> rowBeans = getDataFromXls(fileId);
         for (Bean rowBean : rowBeans) {
@@ -209,7 +156,7 @@ public class ImpUtils {
             }
         }
         return rowBeans;
-    }
+    }*/
 
     /**
      * 通过fileId获取第一个sheet的内容
@@ -218,11 +165,17 @@ public class ImpUtils {
      * @return outBean  rowBeans(List<Bean>)
      * @throws TipException 文件不存在，请重试 / Excel文件解析错误，请校验！
      */
-    public static List<Bean> getDataFromXls(String fileId) {
-        List<Bean> result = new ArrayList<Bean>();
+    public static String getDataFromXls(String fileId,ParamBean paramBean) {
+    	 List<Bean> result = new ArrayList<Bean>();
+       String servId =  paramBean.getServId();//服务名
+       String method = paramBean.getStr("SERVMETHOD");//方法名
         InputStream in = null;
+        OutputStream os = null;
+        TempFile tempFile = null;
+        Bean fileBean=null;
+        Bean  newFileBean  = null;
         try {
-            Bean fileBean = FileMgr.getFile(fileId);
+             fileBean = FileMgr.getFile(fileId);
             in = FileMgr.download(fileBean);
         } catch (Exception e) {
             throw new TipException("文件不存在，请重试");
@@ -231,7 +184,17 @@ public class ImpUtils {
         try {
             workbook = Workbook.getWorkbook(in);
             Sheet sheet1 = workbook.getSheet(0);
+            
+            tempFile = new TempFile(TempFile.Storage.SMART);
+            os = tempFile.getOutputStream();
+
+            log.debug("imp---->open file 1");
+            WritableWorkbook wbook = Workbook.createWorkbook(os, workbook);
+            WritableSheet wSheet = wbook.getSheet(0);
+            
             int rows = sheet1.getRows();
+            //rows大于500时 分批次  
+            //rows小于500一次导出
             for (int i = 0; i < rows; i++) {
                 Cell[] cells = sheet1.getRow(i);
                 Bean rowBean = new Bean();
@@ -241,14 +204,55 @@ public class ImpUtils {
                     rowBean.set(COL_NAME + "" + (j + 1), content);
                 }
                 result.add(rowBean);
+                
+                if(rows<10){
+                	//每五百条进行  一次轮询   对数据进行处理
+                	paramBean.set("datalist", result);
+                	Bean resultres = ServMgr.act(servId,method,paramBean);
+                	List<Bean> rowBeanList = resultres.getList("alllist");
+                	resultres.getList("successlist");
+                	saveErrorAndReturnErrorFile(i,wSheet,sheet1,rowBeanList);
+                }else if(rows>=10){
+                	if(result.size()==10){
+                		//每五百条进行  一次轮询   对数据进行处理
+                    	paramBean.set("datalist", result);
+                    	Bean resultres = ServMgr.act(servId,method,paramBean);
+                    	List<Bean> rowBeanList = resultres.getList("alllist");
+                    	resultres.getList("successlist");
+                    	saveErrorAndReturnErrorFile(i,wSheet,sheet1,rowBeanList);
+                    	result = null;
+                    	result= new ArrayList<Bean>();
+                	}else if(i==rows-1){
+                		//每五百条进行  一次轮询   对数据进行处理
+                    	paramBean.set("datalist", result);
+                    	Bean resultres = ServMgr.act(servId,method,paramBean);
+                    	List<Bean> rowBeanList = resultres.getList("alllist");
+                    	resultres.getList("successlist");
+                    	saveErrorAndReturnErrorFile(i,wSheet,sheet1,rowBeanList);
+                    	result = null;
+                    	result= new ArrayList<Bean>();
+                	}
+                }
             }
+            log.debug("imp---->close File");
+            //关闭文件
+            wbook.write();
+            wbook.close();
+
+            fileBean.set("FILE_NAME", fileBean.getStr("DIS_NAME") + "-导入结果.xls");
+             newFileBean = saveTempFile(fileBean, tempFile);
+
         } catch (Exception e) {
             throw new TipException("Excel文件解析错误，请校验！");
         } finally {
             if (workbook != null) {
                 workbook.close();
             }
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(os);
+            FileMgr.deleteFile(fileId);
+            tempFile.destroy();
         }
-        return result;
+        return newFileBean.getId();
     }
 }
