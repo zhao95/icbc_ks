29161b6d7d9ec0a4a2dc7183827891162a353b46
shiyun <shiyun@ruaho.com>
2017-12-26 16:00:03 +0800

    添加两个 字段  sql记录文件

--- db/increment/2017-12-26.sql
+++ db/increment/2017-12-26.sql
@@ -1,2 +1,6 @@
 -- ts_jklb_jk 表添加字段 LOOK_FLAG
 -- ALTER TABLE `ts_jklb_jk` ADD COLUMN `LOOK_FLAG`  varchar(1) NULL;
+
+-- ts_xmgl_bm_kslbk 添加字段  KSLBK_ALIKE_DIFF KSLBK_TYPE_LEVEL
+-- ALTER TABLE 'ts_xmgl_bm_kslbk' ADD COLUMN 'KSLBK_ALIKE_DIFF' varchar(40) NULL
+-- ALTER TABLE 'ts_xmgl_bm_kslbk' ADD COLUMN 'KSLBK_TYPE_LEVEL' varchar(40) NULL
