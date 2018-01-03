384fe2b26e937fe11ea79d6857719daa600eb994
shenhb <740228413@qq.com>
2018-01-03 13:59:47 +0800

    增量sql调整

--- db/increment/2017-01-02.sql
+++
@@ -1,5 +0,0 @@
-UPDATE ts_xmgl_bm_kslbk set kslbk_type_name = '高级',KSLBK_IDVALUE='高级' WHERE kslbk_type_name = '高级-投行顾问（TH）';
-
-DELETE FROM ts_xmgl_bm_kslbk WHERE kslbk_type_name='高级-投行顾问（ZT）';
-
-UPDATE ts_xmgl_bm_kslbk SET kslbk_type = '1',kslbk_type_name = 'c类',KSLBK_IDVALUE='c类' WHERE  kslbk_id =  '155024vTq17xpi3uQBGu'
