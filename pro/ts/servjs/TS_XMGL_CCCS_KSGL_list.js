4d7438819128f0823fff29a5e0d356522c8608e0
刘远超 <565767474@qq.com>
2018-01-09 15:15:05 +0800

    场次测算 考生管理 导入

--- pro/ts/servjs/TS_XMGL_CCCS_KSGL_list.js
+++ pro/ts/servjs/TS_XMGL_CCCS_KSGL_list.js
@@ -26,3 +26,49 @@ _viewer.afterTreeNodeClick = function(item,id,dictId) {
 _viewer.beforeDelete = function(pkArray) {
 	showVerify(pkArray,_viewer);
 };
+
+
+_viewer.getBtn("imp").unbind("click").bind("click",function() {
+	var xmId = _viewer.getParHandler().getItem("XM_ID").getValue();	
+	var config = {"SERV_ID":_viewer.opts.sId, "FILE_CAT":"EXCEL_UPLOAD", "FILENUMBER":1, 
+		"VALUE":5, "TYPES":"*.xls;*.xlsx", "DESC":"导入Excel文件"};
+	var file = new rh.ui.File({
+		"config" : config,"width":"99%"
+	});
+	
+	var importWin = new rh.ui.popPrompt({
+		title:"请选择文件",
+		tip:"请选择要导入的Excel文件：",
+		okFunc:function() {
+			var fileData = file.getFileData();
+			if (jQuery.isEmptyObject(fileData)) {
+				alert("请选择文件上传");
+				return;
+			}
+			var fileId = null;
+			for (var key in fileData) {
+				fileId = key;
+			}
+			if (fileId == null){
+				alert("请选择文件上传");
+				return;
+			}
+			
+			var param = {};
+			param["XM_ID"] = xmId;
+			
+			_viewer._imp(fileId,param);
+			importWin.closePrompt();
+	        // _viewer.refreshGrid();
+			file.destroy();
+		},
+		closeFunc:function() {
+			file.destroy();
+		}
+	});
+
+    var container = _viewer._getImpContainer(event, importWin);
+	container.append(file.obj);
+	file.obj.css({'margin-left':'5px'});
+	file.initUpload();
+});
