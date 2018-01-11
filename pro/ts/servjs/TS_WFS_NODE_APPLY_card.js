460889c49023347225faaee45547b6eaf87e3301
lianxinshuai <934815805@qq.com>
2018-01-11 09:23:38 +0800

    node的修改

--- pro/ts/servjs/TS_WFS_NODE_APPLY_card.js
+++ pro/ts/servjs/TS_WFS_NODE_APPLY_card.js
@@ -3,7 +3,7 @@ var _viewer = this;
 var WFS_ID=_viewer.getItem("WFS_ID").getValue();
 FireFly.doAct("TS_WFS_APPLY","finds",{"_WHERE_":" and WFS_ID='"+WFS_ID+"'"},true,false,function(data){
 	var flowServ = data._DATA_[0].WFS_SERVID;
-	if(flowServ=="1" || flowServ=="3"){
+	if(flowServ=="1" || flowServ=="2" || flowServ=="3"){
 		_viewer.tabHide("TS_WFS_QJKLC");
 	}else{
 		_viewer.tabHide("TS_WFS_BMSHLC");
