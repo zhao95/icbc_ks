59afe0913a8aecf552987042ca947eae20b58006
王理达 <360607271@qq.com>
2018-01-12 16:06:30 +0800

    sql语句，ts_bm_group_user_dept表添加xm_id字段

--- db/increment/2018-01-12.sql
+++ db/increment/2018-01-12.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `ts_kcgl_update` ADD COLUMN `SHR_NAME` varchar(40) NULL;
 ALTER TABLE `ts_kcgl_update` ADD COLUMN `SHR_ODEPT_NAME` varchar(40) NULL;
+
+-- 项目报名群组人员添加字段
+ALTER TABLE `ts_bm_group_user_dept`
+ADD COLUMN `XM_ID`  varchar(400) NULL AFTER `G_ID`;
