733eeaf13bd4988fc24e14eedafb02ae988b2140
shiyun <shiyun@ruaho.com>
2018-01-26 17:11:01 +0800

    sql

---
+++ db/increment/2018-01-26.sql
@@ -0,0 +1,5 @@
+alter table ts_bmsh_rule_post add column post_type varchar(100) not null;
+alter table ts_bmsh_rule_post add column post_xl varchar(400) not null;
+alter table ts_bmsh_rule_post add column post_defined varchar(40) not null;
+alter table ts_bmsh_rule_post add column post_fuhao varchar(40) not null;
+alter table ts_bmsh_rule_post add column post_duties varchar(400) not null;
