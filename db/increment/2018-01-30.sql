f53cba003bf344655c8a729f322a7b2d00c5bc46
王理达 <360607271@qq.com>
2018-01-30 15:23:37 +0800

    计划添加字段匹配项目类型

--- db/increment/2018-01-30.sql
+++ db/increment/2018-01-30.sql
@@ -1,3 +1,6 @@
 alter table TS_BMSH_RULE add column rule_cengji varchar(40) not null;
 alter table TS_BMSH_RULE add column rule_nowtime varchar(40) not null;
 alter table TS_BMSH_RULE add column rule_lastyear varchar(40) not null;
+
+-- 计划添加字段，匹配项目管理的类型
+ALTER TABLE `ts_jhgl` ADD COLUMN `JH_TYPE_NAME`  varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '计划类型名称，中文' AFTER `JH_LEVEL`;
