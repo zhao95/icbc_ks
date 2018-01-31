88c16a8531d72b0419c57b5d4d44b0c579dd5d5b
shiyun <shiyun@ruaho.com>
2018-01-31 18:12:20 +0800

    sql

--- db/increment/2018-01-31.sql
+++ db/increment/2018-01-31.sql
@@ -1,1 +1,5 @@
 ALTER TABLE ts_xmgl_kcap_dapcc DROP INDEX IDX_XM_KC_CTLG;
+ALTER TABLE TS_BMSH_RULE ADD COLUMN RULE_DESC VARCHAR(40)NOT NULL;
+alter table TS_BMSH_RULE_POST add column POST_ZD VARCHAR(40)NOT NULL;
+alter table TS_BMSH_RULE_POST add column POST_DESC VARCHAR(40)NOT NULL;
+alter table ts_bmsh_rule_kxlgz add column gz_tx_year varchar(40)not null;
