cdc177258858279f400839b8fdd138337687af1b
shiyun <shiyun@ruaho.com>
2018-02-08 11:21:56 +0800

    考试群组指定  考试类别

---
+++ pro/ts/servjs/TS_XMGL_BM_KSQZ_ADMIT_card.js
@@ -0,0 +1,3 @@
+var _viewer = this;
+$("#TS_XMGL_BM_KSQZ_ADMIT-winTabs").find("ul").find("li").eq(1).css("display","none");
+$("#TS_XMGL_BM_KSQZ_ADMIT-winTabs").find("ul").find("li").eq(2).find("a").trigger("click");
